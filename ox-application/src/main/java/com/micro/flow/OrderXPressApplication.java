6d0060f6c5364d66f73de82506c7d2dc08de67d5
maksym_korniev <maksymkorniev88@gmail.com>
2024-03-15 22:55:24 +0200

    created main project and added application module, also added maven configuration file for git

---
+++ ox-application/src/main/java/com/micro/flow/OrderXPressApplication.java
@@ -0,0 +1,13 @@
+package com.micro.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderXPressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderXPressApplication.class, args);
+    }
+
+}
