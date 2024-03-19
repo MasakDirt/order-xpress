d07265415b356d606114702b751759f7be54a90d
maksym_korniev <maksymkorniev88@gmail.com>
2024-03-20 00:48:23 +0200

    added to user module user repository interface for connecting with db directly

---
+++ ox-user/src/main/java/com/micro/flow/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.micro.flow.repository;
+
+import com.micro.flow.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
