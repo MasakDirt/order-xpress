eb5509c28886c3bf969d679d8884846ecc5a2844
maksym_korniev <maksymkorniev88@gmail.com>
2024-03-21 01:25:28 +0200

    added eureka client enabling for main class in user

--- ox-user/src/main/java/com/micro/flow/OxUserApplication.java
+++ ox-user/src/main/java/com/micro/flow/OxUserApplication.java
@@ -2,8 +2,10 @@ package com.micro.flow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OxUserApplication {
 
     public static void main(String[] args) {
