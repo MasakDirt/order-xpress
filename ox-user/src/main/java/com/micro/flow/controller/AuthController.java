482b792d3e1b0190f7c9b8620f38f438acc3667c
maksym_korniev <maksymkorniev88@gmail.com>
2024-03-28 21:59:33 +0200

    created new rest api controller for authorization

---
+++ ox-user/src/main/java/com/micro/flow/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.micro.flow.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+}
