2aba0c9f60845e24e6ee79366468292f1e869b96
maksym_korniev <maksymkorniev88@gmail.com>
2024-04-18 01:43:18 +0300

    created new class for exception handler - ApplicationExceptionHandler

---
+++ ox-clothes/src/main/java/com/micro/flow/exception/ApplicationExceptionHandler.java
@@ -0,0 +1,93 @@
+package com.micro.flow.exception;
+
+import com.micro.flow.dto.ErrorResponse;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.nio.file.AccessDeniedException;
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Slf4j
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorResponse> handleResponseStatusException(
+            HttpServletRequest request, ResponseStatusException ex) {
+        return getErrorResponse(request, ex.getStatusCode(), ex.getReason());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+            HttpServletRequest request, MethodArgumentNotValidException ex) {
+        String message = ex.getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+
+        return getErrorResponse(request, BAD_REQUEST, message);
+    }
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(
+            HttpServletRequest request, ConstraintViolationException ex) {
+        return getErrorResponse(request, BAD_REQUEST, ex.getConstraintName());
+    }
+
+    @ExceptionHandler({HandlerMethodValidationException.class})
+    public ResponseEntity<ErrorResponse> handleHandlerMethodValidationException(
+            HttpServletRequest request, HandlerMethodValidationException ex) {
+        return getErrorResponse(request, BAD_REQUEST, ex.getBody().getDetail());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestExceptions(
+            HttpServletRequest request, RuntimeException ex) {
+        return getErrorResponse(request, BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler({AccessDeniedException.class})
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(
+            HttpServletRequest request, RuntimeException ex) {
+        return getErrorResponse(request, FORBIDDEN, ex.getMessage());
+    }
+
+    @ExceptionHandler({EntityNotFoundException.class})
+    public ResponseEntity<ErrorResponse> handleNotFoundExceptions(
+            HttpServletRequest request, RuntimeException ex) {
+        return getErrorResponse(request, NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(
+            HttpServletRequest request, Exception ex) {
+        return getErrorResponse(request, INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
+
+    private ResponseEntity<ErrorResponse> getErrorResponse(
+            HttpServletRequest request, HttpStatusCode httpStatus, String message) {
+        log.error("Exception raised = {} :: URL = {}", message, request.getRequestURL());
+        return ResponseEntity.status(httpStatus)
+                .body(new ErrorResponse(
+                                LocalDateTime.now(),
+                                httpStatus,
+                                message,
+                                request.getRequestURL().toString()
+                        )
+                );
+    }
+
+}
