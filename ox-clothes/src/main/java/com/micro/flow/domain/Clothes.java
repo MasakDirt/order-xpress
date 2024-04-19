9bd434bc0e31e983a2e25aa534e8b581dc4fa1e7
maksym_korniev <maksymkorniev88@gmail.com>
2024-04-19 16:16:00 +0300

    added clothes entity

--- ox-clothes/src/main/java/com/micro/flow/domain/Clothes.java
+++ ox-clothes/src/main/java/com/micro/flow/domain/Clothes.java
@@ -1,8 +1,6 @@
 package com.micro.flow.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -13,15 +11,23 @@ import lombok.ToString;
 import org.hibernate.annotations.NaturalId;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.FetchType.EAGER;
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Getter
 @Setter
-@MappedSuperclass
+@Entity
+@Table(name = "clothes")
 @ToString(of = "productName")
 @EqualsAndHashCode(of = "productName")
-public abstract class Clothes {
+public class Clothes {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
 
     @NaturalId
     @NotEmpty(message = "Product name must be included!")
@@ -30,6 +36,7 @@ public abstract class Clothes {
 
     @NotNull(message = "Price must be included!")
     @Column(nullable = false)
+    @Min(value = 0, message = "Price cannot be lower that 0!")
     private BigDecimal price;
 
     @Enumerated(STRING)
@@ -45,8 +52,21 @@ public abstract class Clothes {
     @Min(value = 0, message = "Amount of clothes cannot be lower than 0!")
     private int availableAmount;
 
-    public enum Sex {
-        MALE, FEMALE, UNISEX
-    }
+    @Enumerated(STRING)
+    @ElementCollection(fetch = EAGER)
+    @NotEmpty(message = "Sizes must be included!")
+    @CollectionTable(name = "clothes_sizes",
+            joinColumns = @JoinColumn(name = "clothes_id"))
+    private List<SampleSize> sizes;
+
+    @ElementCollection(fetch = EAGER)
+    @NotEmpty(message = "Colors must be included!")
+    @CollectionTable(name = "clothes_colors",
+            joinColumns = @JoinColumn(name = "clothes_id"))
+    private List<String> colors;
+
+    @Enumerated(STRING)
+    @NotNull(message = "Type of clothe must be included!")
+    private Type type;
 
 }
