f2bebd45534cab7804be4aa2468767469a979383
maksym_korniev <maksymkorniev88@gmail.com>
2024-04-20 00:40:57 +0300

    added password encoder to ApplicationConfig

--- ox-user/src/main/java/com/micro/flow/config/ApplicationConfig.java
+++ ox-user/src/main/java/com/micro/flow/config/ApplicationConfig.java
@@ -1,17 +1,26 @@
 package com.micro.flow.config;
 
+import com.micro.flow.client.BagServiceFeignClient;
 import com.micro.flow.repository.UserRepository;
 import com.micro.flow.service.UserService;
 import com.micro.flow.service.impl.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean
-    public UserService userService(UserRepository userRepository) {
-        return new UserServiceImpl(userRepository);
+    public UserService userService(UserRepository userRepository,
+                                   BagServiceFeignClient bagServiceFeignClient) {
+        return new UserServiceImpl(userRepository, bagServiceFeignClient, passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
