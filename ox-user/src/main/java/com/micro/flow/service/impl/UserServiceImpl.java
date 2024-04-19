03af5b84f68d16a94e25f3b8dc0a9b0e5d7a2dd8
maksym_korniev <maksymkorniev88@gmail.com>
2024-04-20 00:42:37 +0300

    added to userService classes createWithBag method

--- ox-user/src/main/java/com/micro/flow/service/impl/UserServiceImpl.java
+++ ox-user/src/main/java/com/micro/flow/service/impl/UserServiceImpl.java
@@ -1,24 +1,27 @@
 package com.micro.flow.service.impl;
 
+import com.micro.flow.client.BagServiceFeignClient;
 import com.micro.flow.domain.User;
 import com.micro.flow.repository.UserRepository;
 import com.micro.flow.service.UserService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static com.micro.flow.domain.User.Role.USER;
 
-@Slf4j
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
-
     private final UserRepository userRepository;
+    private final BagServiceFeignClient bagServiceFeignClient;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
-    public User create(@NotNull User user) {
-        return userRepository.save(user.setRoleAndReturn(USER));
+    public User createWithBag(@NotNull User user) {
+        var created = userRepository.save(user.setRoleAndEncodePasswordAndGet(USER, passwordEncoder));
+        bagServiceFeignClient.createBag(created.getEmail());
+        return created;
     }
 
     @Override
