bc7faea80acf762bf026b41c8d0cbdc70529f394
maksym_korniev <maksymkorniev88@gmail.com>
2024-04-23 00:41:01 +0300

    added new methods for bag service class - 'getById', 'putClothesToBag', 'deleteClothesFromBag'

--- ox-bag/src/main/java/com/micro/flow/service/BagService.java
+++ ox-bag/src/main/java/com/micro/flow/service/BagService.java
@@ -3,9 +3,17 @@ package com.micro.flow.service;
 import com.micro.flow.domain.Bag;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public interface BagService {
 
     Bag create(String userEmail);
+
+    Bag getById(UUID id);
+
+    void putClothesToBag(UUID id, Long clothesId);
+
+    void deleteClothesFromBag(UUID id, Long clothesId);
 
 }
--- ox-bag/src/main/java/com/micro/flow/service/iml/BagServiceImpl.java
+++ ox-bag/src/main/java/com/micro/flow/service/iml/BagServiceImpl.java
@@ -1,13 +1,20 @@
 package com.micro.flow.service.iml;
 
+import com.micro.flow.client.ClothesServiceFeignClients;
 import com.micro.flow.domain.Bag;
+import com.micro.flow.dto.clothes.ClothesResponse;
 import com.micro.flow.repository.BagRepository;
 import com.micro.flow.service.BagService;
 import lombok.AllArgsConstructor;
 
+import java.math.BigDecimal;
+import java.util.Set;
+import java.util.UUID;
+
 @AllArgsConstructor
 public class BagServiceImpl implements BagService {
     private final BagRepository bagRepository;
+    private final ClothesServiceFeignClients serviceFeignClients;
 
     @Override
     public Bag create(String userEmail) {
@@ -15,6 +22,34 @@ public class BagServiceImpl implements BagService {
         bag.setUserEmail(userEmail);
 
         return bagRepository.save(bag);
+    }
+
+    @Override
+    public Bag getById(UUID id) {
+        return bagRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Bag not found"));
+    }
+
+    @Override
+    public void putClothesToBag(UUID id, Long clothesId) {
+        setPriceAndSave(getById(id).updateClothesIdAndGet(clothesId));
+    }
+
+    @Override
+    public void deleteClothesFromBag(UUID id, Long clothesId) {
+        setPriceAndSave(getById(id).deleteClothesIdAndGet(clothesId));
+    }
+
+    private void setPriceAndSave(Bag bag) {
+        bag.setTotalPrice(reducePriceForBag(bag.getClothesIds()));
+        bagRepository.save(bag);
+    }
+
+    private BigDecimal reducePriceForBag(Set<Long> clothesIds) {
+        return serviceFeignClients.getClothesByIds(clothesIds)
+                .stream()
+                .map(ClothesResponse::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
 }
