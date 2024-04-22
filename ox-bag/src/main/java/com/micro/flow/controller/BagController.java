d6009a764c805582933197a9ee5341270e14f197
maksym_korniev <maksymkorniev88@gmail.com>
2024-04-23 00:40:12 +0300

    created new methods in controller class - 'create', 'addClothesToBag', 'deleteClothesFromBag'

--- ox-bag/src/main/java/com/micro/flow/controller/BagController.java
+++ ox-bag/src/main/java/com/micro/flow/controller/BagController.java
@@ -1,15 +1,13 @@
 package com.micro.flow.controller;
 
-import com.micro.flow.client.ClothesServiceFeignClients;
-import com.micro.flow.client.UserServiceFeignClient;
-import com.micro.flow.mapper.BagMapper;
 import com.micro.flow.service.BagService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -17,14 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/my-bag")
 public class BagController {
     private final BagService bagService;
-    private final BagMapper bagMapper;
-    private final UserServiceFeignClient userServiceFeignClient;
-    private final ClothesServiceFeignClients clothesServiceFeignClients;
 
-    @PostMapping("/{email}")
-    public void create(@PathVariable("email") String email) {
+    @PostMapping("/{user-email}")
+    public ResponseEntity<UUID> create(@PathVariable("user-email") String email) {
         var bag = bagService.create(email);
-        log.info("CREATED BAG: {} - for user - {}", bag, email);
+        log.info("CREATED-BAG: {} - for user - {}", bag, email);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(bag.getId());
+    }
+
+    @PutMapping("/{id}/clothes/{clothes-id}")
+    public void addClothesToBag(@PathVariable("id") UUID id, @PathVariable("clothes-id") Long clothesId) {
+        bagService.putClothesToBag(id, clothesId);
+        log.info("PUT-BAG === id - {} == clothes: {}", id, clothesId);
+    }
+
+    @DeleteMapping("/{id}/clothes/{clothes-id}")
+    public void deleteClothesFromBag(@PathVariable("id") UUID id, @PathVariable("clothes-id") Long clothesId) {
+        bagService.deleteClothesFromBag(id, clothesId);
+        log.info("DELETE-BAG === id - {} == clothes: {}", id, clothesId);
     }
 
 }
