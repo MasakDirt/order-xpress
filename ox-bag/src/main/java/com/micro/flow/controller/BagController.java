6ca51151c8fddb5c13b5d1be9a64be04ad14ce80
maksym_korniev <maksymkorniev88@gmail.com>
2024-04-24 18:42:59 +0300

    added request for getting one bag

--- ox-bag/src/main/java/com/micro/flow/controller/BagController.java
+++ ox-bag/src/main/java/com/micro/flow/controller/BagController.java
@@ -1,5 +1,7 @@
 package com.micro.flow.controller;
 
+import com.micro.flow.dto.BagResponse;
+import com.micro.flow.mapper.BagMapper;
 import com.micro.flow.service.BagService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,27 +16,43 @@ import java.util.UUID;
 @AllArgsConstructor
 @RequestMapping("/api/v1/my-bag")
 public class BagController {
+
     private final BagService bagService;
+    private final BagMapper bagMapper;
 
     @PostMapping("/{user-email}")
     public ResponseEntity<UUID> create(@PathVariable("user-email") String email) {
         var bag = bagService.create(email);
-        log.info("CREATED-BAG: {} - for user - {}", bag, email);
+        log.debug("CREATED-BAG: {} - for user - {}", bag, email);
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(bag.getId());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<BagResponse> getById(@PathVariable("id") UUID id) {
+        var response = bagMapper.getBagResponseFromDomain(bagService.getById(id));
+        log.debug("GET BAG: by id - {}", id);
+
+        return ResponseEntity.ok(response);
+    }
+
     @PutMapping("/{id}/clothes/{clothes-id}")
-    public void addClothesToBag(@PathVariable("id") UUID id, @PathVariable("clothes-id") Long clothesId) {
+    public ResponseEntity<String> addClothesToBag(
+            @PathVariable("id") UUID id, @PathVariable("clothes-id") Long clothesId) {
         bagService.putClothesToBag(id, clothesId);
-        log.info("PUT-BAG === id - {} == clothes: {}", id, clothesId);
+        log.debug("PUT-BAG === id - {} == clothes: {}", id, clothesId);
+
+        return ResponseEntity.ok("You added new clothe to your bag!");
     }
 
     @DeleteMapping("/{id}/clothes/{clothes-id}")
-    public void deleteClothesFromBag(@PathVariable("id") UUID id, @PathVariable("clothes-id") Long clothesId) {
+    public ResponseEntity<String> deleteClothesFromBag(
+            @PathVariable("id") UUID id, @PathVariable("clothes-id") Long clothesId) {
         bagService.deleteClothesFromBag(id, clothesId);
-        log.info("DELETE-BAG === id - {} == clothes: {}", id, clothesId);
+        log.debug("DELETE-BAG === id - {} == clothes: {}", id, clothesId);
+
+        return ResponseEntity.ok("You deleted clothe from your bag!");
     }
 
 }
