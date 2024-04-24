b71f5f4de253b893763e6bd06340a62c2918313e
maksym_korniev <maksymkorniev88@gmail.com>
2024-04-24 18:41:38 +0300

    created BagMapperImpl class which implement mapping method - 'getBagResponseFromDomain'

---
+++ ox-bag/src/main/java/com/micro/flow/mapper/impl/BagMapperImpl.java
@@ -0,0 +1,52 @@
+package com.micro.flow.mapper.impl;
+
+import com.micro.flow.client.ClothesServiceFeignClients;
+import com.micro.flow.client.UserServiceFeignClient;
+import com.micro.flow.domain.Bag;
+import com.micro.flow.dto.BagResponse;
+import com.micro.flow.dto.clothes.ClothesResponse;
+import com.micro.flow.dto.user.UserResponse;
+import com.micro.flow.mapper.BagMapper;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Set;
+
+@AllArgsConstructor
+public class BagMapperImpl implements BagMapper {
+
+    private final UserServiceFeignClient userServiceFeignClient;
+    private final ClothesServiceFeignClients clothesServiceFeignClients;
+
+    @Override
+    public BagResponse getBagResponseFromDomain(Bag bag) {
+        BagResponse.BagResponseBuilder bagResponse = BagResponse.builder();
+
+        if (bag.getId() != null) {
+            bagResponse.id(bag.getId());
+        }
+
+        bagResponse.totalPrice(bag.getTotalPrice());
+
+        if (bag.getUserEmail() != null && !bag.getUserEmail().trim().isEmpty()) {
+            bagResponse.userResponse(getUserResponse(bag.getUserEmail()));
+        }
+
+        if (bag.getClothesIds() != null && !bag.getClothesIds().isEmpty()) {
+            bagResponse.clothesResponses(getClothesResponses(bag.getClothesIds()));
+        } else {
+            bagResponse.clothesResponses(List.of());
+        }
+
+        return bagResponse.build();
+    }
+
+    private UserResponse getUserResponse(String userEmail) {
+        return userServiceFeignClient.getByEmail(userEmail);
+    }
+
+    private List<ClothesResponse> getClothesResponses(Set<Long> clothesIds) {
+        return clothesServiceFeignClients.getClothesByIds(clothesIds);
+    }
+
+}
