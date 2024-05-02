673d18ef2d82984abbf9a0fc4ca170a144290920
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-02 21:37:44 +0300

    removed security from pom and added security config for user module

---
+++ ox-user/src/main/java/com/micro/flow/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.micro.flow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request ->
+                request.requestMatchers("/api/v1/**").permitAll()
+                        .anyRequest()
+                        .permitAll());
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+}
