89f22d69c1d152e476bb725ffa2ebda91f690e43
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-06 14:09:13 +0300

    added to user data some new and updated values

--- ox-user/src/main/resources/scripts/data.sql
+++ ox-user/src/main/resources/scripts/data.sql
@@ -1,3 +1,5 @@
-INSERT INTO users (username, email, password, user_role)
-VALUES ('admin', 'admin@mail.co', '$2a$10$bIjGIiFFRtxMLutKDvuPTeTtpROhRceXImnIqzOUkkwN/aBMJE/hu', 'ADMIN'),
-       ('user', 'user@mail.co', '$2a$10$bIjGIiFFRtxMLutKDvuPTeTtpROhRceXImnIqzOUkkwN/aBMJE/hu', 'USER');
+INSERT INTO users (username, email, password, user_role) -- encoded password - 1234 for all users
+VALUES ('adminito25', 'admin@mail.co', '$2a$10$bIjGIiFFRtxMLutKDvuPTeTtpROhRceXImnIqzOUkkwN/aBMJE/hu', 'ADMIN'),
+       ('loperlo', 'loperlo@mail.co', '$2a$10$bIjGIiFFRtxMLutKDvuPTeTtpROhRceXImnIqzOUkkwN/aBMJE/hu', 'USER'),
+       ('mamamiya', 'helloworld@mail.co', '$2a$10$bIjGIiFFRtxMLutKDvuPTeTtpROhRceXImnIqzOUkkwN/aBMJE/hu', 'USER'),
+       ('bibus', 'incognito@mail.co', '$2a$10$bIjGIiFFRtxMLutKDvuPTeTtpROhRceXImnIqzOUkkwN/aBMJE/hu', 'ADMIN');
