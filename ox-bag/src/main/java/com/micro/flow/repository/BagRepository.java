6d1cbe7d3c36537265804b4c2d0208d20198f08f
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-06 14:01:58 +0300

    BagRepository added new method - 'findByUserEmail'

--- ox-bag/src/main/java/com/micro/flow/repository/BagRepository.java
+++ ox-bag/src/main/java/com/micro/flow/repository/BagRepository.java
@@ -3,7 +3,11 @@ package com.micro.flow.repository;
 import com.micro.flow.domain.Bag;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface BagRepository extends CrudRepository<Bag, UUID> {
+
+    Optional<Bag> findByUserEmail(String userEmail);
+
 }
