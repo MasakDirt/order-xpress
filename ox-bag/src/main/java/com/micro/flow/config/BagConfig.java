f8b2e6af4ec97951563a96c853786ca22a89c4c5
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-06 14:07:43 +0300

    BagConfig remove bean for bagService

--- ox-bag/src/main/java/com/micro/flow/config/BagConfig.java
+++ ox-bag/src/main/java/com/micro/flow/config/BagConfig.java
@@ -4,20 +4,11 @@ import com.micro.flow.client.ClothesServiceFeignClients;
 import com.micro.flow.client.UserServiceFeignClient;
 import com.micro.flow.mapper.BagMapper;
 import com.micro.flow.mapper.impl.BagMapperImpl;
-import com.micro.flow.repository.BagRepository;
-import com.micro.flow.service.BagService;
-import com.micro.flow.service.iml.BagServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BagConfig {
-
-    @Bean
-    public BagService bagService(BagRepository bagRepository,
-                                 ClothesServiceFeignClients clothesServiceFeignClients) {
-        return new BagServiceImpl(bagRepository, clothesServiceFeignClients);
-    }
 
     @Bean
     public BagMapper bagMapper(ClothesServiceFeignClients clothesServiceFeignClients,
