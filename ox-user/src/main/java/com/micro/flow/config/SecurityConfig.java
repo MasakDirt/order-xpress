8826b1e4dd7e5da91c6ac75ea6f9b553984449a6
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-06 14:10:03 +0300

    added to SecurityConfig bean for password encoder

--- ox-user/src/main/java/com/micro/flow/config/SecurityConfig.java
+++ ox-user/src/main/java/com/micro/flow/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,6 +26,12 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
