cbbe7ddb11663c33d94352218ee9fef4e0368665
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-06 14:11:34 +0300

    Added logging for UserServiceImpl

--- ox-user/src/main/java/com/micro/flow/service/impl/UserServiceImpl.java
+++ ox-user/src/main/java/com/micro/flow/service/impl/UserServiceImpl.java
@@ -7,10 +7,15 @@ import com.micro.flow.service.UserService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.micro.flow.domain.User.Role.USER;
 
+@Slf4j
+@Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
@@ -18,22 +23,30 @@ public class UserServiceImpl implements UserService {
     private final PasswordEncoder passwordEncoder;
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public User createWithBag(@NotNull User user) {
-        var created = userRepository.save(user.setRoleAndEncodePasswordAndGet(USER, passwordEncoder));
+        var created = userRepository.save(user.setRoleAndEncodePassword(USER, passwordEncoder));
+        log.info("Created user: {}", created);
         bagServiceFeignClient.createBag(created.getEmail());
+        log.info("Sent request for BAG creation!");
         return created;
     }
 
     @Override
     public User readByEmail(String email) {
-        return userRepository.findByEmail(email).orElseThrow(
-                () -> new EntityNotFoundException("User not found"));
+        var user = userRepository.findByEmail(email).orElseThrow(
+                () -> new EntityNotFoundException(String.format("User with email %s not found," +
+                        " please check if you provided valid e-mail!", email)));
+        log.info("Read user by email: {}", email);
+        return user;
     }
 
     @Override
     public User readById(Long id) {
-        return userRepository.findById(id).orElseThrow(
+        var user = userRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("User not found"));
+        log.info("Read user by id: {}", id);
+        return user;
     }
 
 }
