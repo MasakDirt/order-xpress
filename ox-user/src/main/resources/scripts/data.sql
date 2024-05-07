12a3a66fd474372d42ea791ca9c9bab97bd30541
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-07 22:23:00 +0300

    added data for account table

--- ox-user/src/main/resources/scripts/data.sql
+++ ox-user/src/main/resources/scripts/data.sql
@@ -3,3 +3,9 @@ VALUES ('adminito25', 'admin@mail.co', '$2a$10$bIjGIiFFRtxMLutKDvuPTeTtpROhRceXI
        ('loperlo', 'loperlo@mail.co', '$2a$10$bIjGIiFFRtxMLutKDvuPTeTtpROhRceXImnIqzOUkkwN/aBMJE/hu', 'USER'),
        ('mamamiya', 'helloworld@mail.co', '$2a$10$bIjGIiFFRtxMLutKDvuPTeTtpROhRceXImnIqzOUkkwN/aBMJE/hu', 'USER'),
        ('bibus', 'incognito@mail.co', '$2a$10$bIjGIiFFRtxMLutKDvuPTeTtpROhRceXImnIqzOUkkwN/aBMJE/hu', 'ADMIN');
+
+INSERT INTO account(balance, user_id)
+VALUES (100.00, 1),
+       (0.30, 2),
+       (2305.20, 3),
+       (22.03, 4);
