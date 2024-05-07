83cc231b0cd1c6a7ca277b8950fb988b32f97b9a
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-07 22:21:32 +0300

    created new table - account with balance value and user id reference

--- ox-user/src/main/resources/scripts/schema.sql
+++ ox-user/src/main/resources/scripts/schema.sql
@@ -1,10 +1,21 @@
+ALTER TABLE IF EXISTS account
+DROP CONSTRAINT IF EXISTS FK_account_users;
+
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS account;
 
 CREATE TABLE users
 (
     id BIGSERIAL PRIMARY KEY,
-    username  VARCHAR(255) NOT NULL,
-    email     VARCHAR(255) NOT NULL UNIQUE,
-    password  VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     user_role VARCHAR(255) NOT NULL CHECK (user_role IN ('ADMIN', 'USER'))
+);
+
+CREATE TABLE account
+(
+    id BIGSERIAL PRIMARY KEY,
+    balance NUMERIC(10, 2),
+    user_id BIGINT REFERENCES users(id)
 );
