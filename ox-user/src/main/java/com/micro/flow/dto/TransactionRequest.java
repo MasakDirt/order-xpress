8d99e273cce93c809bcd9e6a466fce37dda25dd5
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-11 16:02:47 +0300

    added TransactionRequest to user module

---
+++ ox-user/src/main/java/com/micro/flow/dto/TransactionRequest.java
@@ -0,0 +1,18 @@
+package com.micro.flow.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionRequest {
+
+    private BigDecimal amount;
+
+}
