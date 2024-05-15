770a5ef39a7da58950bab95673db02ece622e14a
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-15 23:21:08 +0300

    moved AccountRepository from user module to account module

--- ox-account/src/main/java/com/micro/flow/repository/AccountRepository.java
+++ ox-account/src/main/java/com/micro/flow/repository/AccountRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    Optional<Account> findByUserEmail(String userEmail);
+    Optional<Account> findByUsername(String username);
 
 }
