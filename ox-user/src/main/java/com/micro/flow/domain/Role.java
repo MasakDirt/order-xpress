ab1017a62654eb3434f1ba1183698f5fc26baf35
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-19 00:18:24 +0300

    created role class for authorities

---
+++ ox-user/src/main/java/com/micro/flow/domain/Role.java
@@ -0,0 +1,41 @@
+package com.micro.flow.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "keycloak_role")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "name")
+@ToString(of = "name")
+public class Role implements GrantedAuthority {
+
+    @Id
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "realm_id")
+    private String realmId;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + name;
+    }
+
+}
