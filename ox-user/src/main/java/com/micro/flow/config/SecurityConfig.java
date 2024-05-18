ced566785fc76dc7324baffb56fa7853dedf211c
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-19 00:19:03 +0300

    removed password encoder bean from SecurityConfig and also added config for keycloak

--- ox-user/src/main/java/com/micro/flow/config/SecurityConfig.java
+++ ox-user/src/main/java/com/micro/flow/config/SecurityConfig.java
@@ -1,37 +1,37 @@
 package com.micro.flow.config;
 
+import com.micro.flow.component.JwtConverter;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+    private final JwtConverter jwtConverter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request ->
-                request.requestMatchers("/api/v1/**").permitAll()
-                        .anyRequest()
-                        .permitAll());
-        http
+                        request.requestMatchers("/api/v1/auth/**").permitAll()
+                                .anyRequest()
+                                .authenticated())
                 .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable);
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sess ->
+                        sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer ->
+                        jwtConfigurer.jwtAuthenticationConverter(jwtConverter)));
 
         return http.build();
-    }
-
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
 }
