f9974b6f839c47cac3a30d652764e93fd902fbe3
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-20 00:01:57 +0300

    added to UserService 'createWithBag' method password param

--- ox-user/src/main/java/com/micro/flow/service/UserService.java
+++ ox-user/src/main/java/com/micro/flow/service/UserService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public interface UserService {
 
-    User createWithBag(User user);
+    User createWithBag(User user, String password);
 
     User readByUsername(String username);
 
