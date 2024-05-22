1d97c0f374da5741c8ed432ae796ea00c23ff233
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-22 23:47:18 +0300

    refactor UserServiceImpl class with removing equals method and adding new one for it

--- ox-user/src/main/java/com/micro/flow/domain/User.java
+++ ox-user/src/main/java/com/micro/flow/domain/User.java
@@ -1,7 +1,6 @@
 package com.micro.flow.domain;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.EqualsAndHashCode;
--- ox-user/src/main/java/com/micro/flow/service/impl/UserServiceImpl.java
+++ ox-user/src/main/java/com/micro/flow/service/impl/UserServiceImpl.java
@@ -45,7 +45,7 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public User create(@NotNull User user, String password) {
-        user.setImportantValues(realmId, Set.of(roleRepository.findByName("ox_user")));
+        user.setImportantValues(realmId, Set.of(roleRepository.getByName("ox_user")));
         var created = userRepository.save(user);
         log.info("Created: {}", created);
         credentialService.createCredentialForUser(created, password);
@@ -63,25 +63,18 @@ public class UserServiceImpl implements UserService {
     private void tryCreateBag(String username) {
         UUID bagId = bagServiceFeignClient.createBag(username);
         log.info("Sent request for BAG creation!");
-        checkBagForNull(bagId);
-    }
-
-    private void checkBagForNull(UUID bagId) {
-        if (bagId == null) {
-            log.error("Can't get bag id!");
-            throw new IllegalArgumentException("Sorry our mistake. We are already working on it.");
-        }
+        checkFeignClientResponseForNull(bagId, "bag");
     }
 
     private void tryCreateAccount(String username) {
         Long id = accountServiceFeignClient.createAccount(username);
         log.info("Sent request for ACCOUNT creation!");
-        checkAccountIdForNull(id);
+        checkFeignClientResponseForNull(id, "account");
     }
 
-    private void checkAccountIdForNull(Long id) {
+    private <T> void checkFeignClientResponseForNull(T id, String feignClientName) {
         if (id == null) {
-            log.error("Can't get account id!");
+            log.error("Can't get {} id!", feignClientName);
             throw new IllegalArgumentException("Sorry our mistake. We are already working on it.");
         }
     }
