a0539cbb4bcf33fcfe6f8b1b8d81b1564a09b5ef
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-25 01:42:12 +0300

    refactor account module Mapper where I define which id is took for response

--- ox-account/src/main/java/com/micro/flow/mapper/AccountMapper.java
+++ ox-account/src/main/java/com/micro/flow/mapper/AccountMapper.java
@@ -4,10 +4,12 @@ import com.micro.flow.domain.Account;
 import com.micro.flow.dto.AccountResponse;
 import com.micro.flow.dto.UserDtoForAccount;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
 
+    @Mapping(target = "id", source = "account.id")
     AccountResponse getResponseFromDomain(Account account, UserDtoForAccount userDtoForAccount);
 
 }
