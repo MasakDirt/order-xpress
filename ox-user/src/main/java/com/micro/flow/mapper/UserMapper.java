5cd31fba0bc8e7109840b1c3561747d9e2529dfe
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-25 01:49:39 +0300

    added to user response mapping email verifying value

--- ox-user/src/main/java/com/micro/flow/mapper/UserMapper.java
+++ ox-user/src/main/java/com/micro/flow/mapper/UserMapper.java
@@ -6,6 +6,7 @@ import com.micro.flow.dto.UserCreateRequest;
 import com.micro.flow.dto.UserDtoForAccount;
 import com.micro.flow.dto.UserResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -13,6 +14,7 @@ import java.util.stream.Collectors;
 @Mapper(componentModel = "spring", imports = Role.class)
 public interface UserMapper {
 
+    @Mapping(target = "isEmailVerified", expression = "java(user.isEmailVerified())")
     UserResponse getUserResponseFromDomain(User user);
 
     User getUserFromCreateRequest(UserCreateRequest createRequest);
