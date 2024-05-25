39bfe5daed52e368462d62b92a3fc95294b4826a
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-26 00:38:00 +0300

    Totally refactor BagController used username instead of id

--- ox-bag/src/main/java/com/micro/flow/controller/BagController.java
+++ ox-bag/src/main/java/com/micro/flow/controller/BagController.java
@@ -7,7 +7,14 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -29,44 +36,51 @@ public class BagController {
                 .body(bag.getId());
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<BagResponse> getById(@PathVariable("id") UUID id) {
-        var response = bagMapper.getBagResponseFromDomain(bagService.getById(id));
-        log.debug("GET BAG: by id - {}", id);
+    @GetMapping("/{username}")
+    @PreAuthorize("@globalAuthService.isUserAuthenticated(#username, authentication.name)")
+    public ResponseEntity<BagResponse> getById(@PathVariable("username") String username) {
+        var response = bagMapper.getBagResponseFromDomain(bagService.getByUsername(username));
+        log.debug("GET BAG: by username - {}", username);
 
         return ResponseEntity.ok(response);
     }
 
-    @PutMapping("/{id}/clothes/{clothes-id}")
+    @PutMapping("/{username}/clothes/{clothes-id}")
+    @PreAuthorize("@globalAuthService.isUserAuthenticated(#username, authentication.name)")
     public ResponseEntity<String> addClothesToBag(
-            @PathVariable("id") UUID id, @PathVariable("clothes-id") Long clothesId) {
-        bagService.putClothesToBag(id, clothesId);
-        log.debug("PUT-BAG === id - {} == clothes: {}", id, clothesId);
+            @PathVariable("username") String username,
+            @PathVariable("clothes-id") Long clothesId) {
+        bagService.putClothesToBag(username, clothesId);
+        log.debug("PUT-BAG === username - {} == clothes: {}", username, clothesId);
 
         return ResponseEntity.ok("You added new clothe to your bag!");
     }
 
-    @DeleteMapping("/{id}/clothes/{clothes-id}")
+    @DeleteMapping("/{username}/clothes/{clothes-id}")
+    @PreAuthorize("@globalAuthService.isUserAuthenticated(#username, authentication.name)")
     public ResponseEntity<String> deleteClothesFromBag(
-            @PathVariable("id") UUID id, @PathVariable("clothes-id") Long clothesId) {
-        bagService.deleteClothesFromBag(id, clothesId);
-        log.debug("DELETE-BAG === id - {} == clothes: {}", id, clothesId);
+            @PathVariable("username") String username,
+            @PathVariable("clothes-id") Long clothesId) {
+        bagService.deleteClothesFromBag(username, clothesId);
+        log.debug("DELETE-BAG === username - {} == clothes: {}", username, clothesId);
 
         return ResponseEntity.ok("You deleted clothe from your bag!");
     }
 
-    @GetMapping("/{id}/total-price")
-    public ResponseEntity<BigDecimal> getTotalPrice(@PathVariable("id") UUID id) {
-        var totalPrice = bagService.getTotalPrice(id);
-        log.debug("GET-BAG-TOTAL_PRICE: id - {}", id);
+    @GetMapping("/{username}/total-price")
+    @PreAuthorize("@globalAuthService.isUserAuthenticated(#username, authentication.name)")
+    public ResponseEntity<BigDecimal> getTotalPrice(@PathVariable("username") String username) {
+        var totalPrice = bagService.getTotalPriceByUsername(username);
+        log.debug("GET-BAG-TOTAL_PRICE: username - {}", username);
 
         return ResponseEntity.ok(totalPrice);
     }
 
-    @PostMapping("/{id}/reset-bag")
-    public void resetBag(@PathVariable("id") UUID id) {
-        bagService.resetBag(id);
-        log.debug("EMPTY-BAG: id - {}", id);
+    @PostMapping("/{username}/reset-bag")
+    @PreAuthorize("@globalAuthService.isUserAuthenticated(#username, authentication.name)")
+    public void resetBag(@PathVariable("username") String username) {
+        bagService.resetBag(username);
+        log.debug("EMPTY-BAG: username - {}", username);
     }
 
 }
