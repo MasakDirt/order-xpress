e9f47632e7388700590555cb6cfbd4ab39492df4
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-26 00:40:53 +0300

    refactor UserServiceFeignClient in bag module for getting it by username

--- ox-bag/src/main/java/com/micro/flow/client/UserServiceFeignClient.java
+++ ox-bag/src/main/java/com/micro/flow/client/UserServiceFeignClient.java
@@ -1,24 +1,27 @@
 package com.micro.flow.client;
 
 import com.micro.flow.dto.user.UserResponse;
+import com.micro.flow.oauth2config.FeignConfig;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ox-user")
+import java.util.Set;
+
+@FeignClient(name = "ox-user", configuration = FeignConfig.class)
 public interface UserServiceFeignClient {
 
-    @GetMapping("/api/v1/users/e/{email}")
-    @CircuitBreaker(name = "ox-user", fallbackMethod = "fallbackUser")
-    UserResponse getByEmail(@PathVariable("email") String email);
+    @GetMapping("/api/v1/users/u/{username}")
+    @CircuitBreaker(name = "ox-user-get", fallbackMethod = "fallbackUser")
+    UserResponse getByUsername(@PathVariable("username") String username);
 
-    default UserResponse fallbackUser(String email, Throwable throwable) {
+    default UserResponse fallbackUser(String username, Throwable throwable) {
         return UserResponse.builder()
-                .email(email)
-                .role("HIDDEN")
-                .username("USERNAME")
-                .id(0L)
+                .email("EMAIL")
+                .roles(Set.of("HIDDEN"))
+                .username(username)
+                .id("HIDDEN")
                 .build();
     }
 
