720e6952b004c4346b118849b41999e6810934fe
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-26 00:37:13 +0300

    UserServiceFeignClient in account module add configuration for it

--- ox-account/src/main/java/com/micro/flow/client/UserServiceFeignClient.java
+++ ox-account/src/main/java/com/micro/flow/client/UserServiceFeignClient.java
@@ -1,7 +1,7 @@
 package com.micro.flow.client;
 
-import com.micro.flow.config.FeignConfig;
 import com.micro.flow.dto.UserDtoForAccount;
+import com.micro.flow.oauth2config.FeignConfig;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
