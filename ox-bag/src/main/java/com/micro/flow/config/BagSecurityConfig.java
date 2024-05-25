46a6b5313e342e0882d725e0ad7c46ac792b3328
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-26 00:38:39 +0300

    removed unnecessary annotation form bag security

--- ox-bag/src/main/java/com/micro/flow/config/BagSecurityConfig.java
+++ ox-bag/src/main/java/com/micro/flow/config/BagSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.micro.flow.config;
 
 import com.micro.flow.oauth2component.JwtConverter;
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -14,7 +13,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-@AllArgsConstructor
 public class BagSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http,
