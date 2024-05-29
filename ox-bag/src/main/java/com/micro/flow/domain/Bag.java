64ace4938030c4258426ada886719dfef34ac3fe
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-30 00:05:37 +0300

    refactor in Bag domain username check values

--- ox-bag/src/main/java/com/micro/flow/domain/Bag.java
+++ ox-bag/src/main/java/com/micro/flow/domain/Bag.java
@@ -3,6 +3,7 @@ package com.micro.flow.domain;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -33,7 +34,10 @@ public class Bag {
 
     @Column(value = "user_email")
     @Indexed(value = "idx_user_email")
-    @NotEmpty(message = "Bag must contain owner!")
+    @NotNull(message = "Username must be lowercase and can contain only letters," +
+            " numbers, underscores, and dots")
+    @Pattern(regexp = "^[a-z0-9_.]+$", message = "Username must be lowercase" +
+            " and can contain only letters, numbers, underscores, and dots")
     private String username;
 
     @Setter(AccessLevel.PRIVATE)
