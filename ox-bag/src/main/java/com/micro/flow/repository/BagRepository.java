670ba45617a89c1ad45310039b6e9e1d0403088c
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-30 14:59:19 +0300

    added repository annotation to BagRepository

--- ox-bag/src/main/java/com/micro/flow/repository/BagRepository.java
+++ ox-bag/src/main/java/com/micro/flow/repository/BagRepository.java
@@ -2,10 +2,12 @@ package com.micro.flow.repository;
 
 import com.micro.flow.domain.Bag;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface BagRepository extends CrudRepository<Bag, UUID> {
 
     Optional<Bag> findByUsername(String username);
